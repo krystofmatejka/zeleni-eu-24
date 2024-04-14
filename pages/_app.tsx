09a344c09022843549c1aa39d40b5fca5329803b
Kryštof Matějka <kmatejka@microsoft.com>
2024-04-14 16:06:43 +0200

    fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
-import { Transition } from "./transition"
+import { Transition } from "../src/transition"
 import styles from "./styles.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     console.log('app', styles)
-  return <Transition><Component {...pageProps} /></Transition>
+    return <Transition><Component {...pageProps} /></Transition>
 }
