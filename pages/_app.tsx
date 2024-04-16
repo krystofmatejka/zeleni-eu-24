8692baa8d6be228281149924e7dceaaffe6bff05
Kryštof Matějka <kmatejka@microsoft.com>
2024-04-16 19:02:47 +0200

    add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import type { AppProps } from 'next/app'
+import { Lato } from 'next/font/google'
 import { Transition } from "../src/transition"
 import styles from "./styles.css";
 
+const lato = Lato({
+  weight: ['300', '700'],
+  subsets: ['latin-ext'],
+  variable: '--font-lato',
+  display: 'swap',
+})
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-    console.log('app', styles)
-    return <Transition><Component {...pageProps} /></Transition>
+    console.log('app', styles, lato)
+    return (
+        <>
+        <style jsx global>
+{`html {
+    font-family: ${lato.style.fontFamily};
+    --font-lato: ${lato.style.fontFamily};
+}
+`}</style>
+            <Transition>
+                <Component {...pageProps} />
+            </Transition>
+        </>
+    )
 }
