a2b7cf696b01452aeaeb5857751e34df0b7b6150
Kryštof Matějka <kmatejka@microsoft.com>
2024-04-17 07:52:38 +0200

    flip animation

--- src/transition.tsx
+++ src/transition.tsx
@@ -9,12 +9,9 @@ export const Transition = ({ children }: { children: React.ReactNode }) => {
         <motion.div
           key={pathname}
           initial= {{ opacity: 1, y: '0%' }}
-          animate={{ opacity: 1, y: ['-100%', '0%',] }}
-          exit={{ opacity: 0, y: '100%' }}
-          transition={{
-            type: "linear",
-            duration: 0.3,
-          }}
+          animate={{ opacity: 1, y: ['100%', '0%'], transition: { duration: 0.3} }}
+          exit={{ opacity: 0, y: '-100%', transition: { duration: 0.15} }}
+          transition={{ type: "linear" }}
           style={{height: '100%'}}
         >
           {children}
